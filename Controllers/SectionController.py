c9e2b97c992a5b9fec14756ffcfd2c563a318dbf
rafa-quiles <rafael.quiles1@upr.edu>
2024-11-09 13:06:10 -0400

    added comprehensive errors messages to section controller

--- Controllers/SectionController.py
+++ Controllers/SectionController.py
@@ -24,12 +24,95 @@ class SectionController:
     
     def InsertSection(self,data):
         try:
+            roomid = int(data["roomid"])
+            if (roomid < 0):
+                return {"error": f"Invalid roomid {roomid} is below 0. roomid cannot be a negative number."}    
+        except ValueError:
+            return {"error": f"Invalid roomid: {data['roomid']} is not a number"}, 400
+        
+        try:
+            cid = int(data["cid"])
+            if (cid < 0):
+                return {"error": f"Invalid cid {cid} is below 0. cid cannot be a negative number."}    
+        except ValueError:
+            return {"error": f"Invalid cid: {data['cid']} is not a number"}, 400
+        
+        try:
+            mid = int(data["mid"])
+            if (mid < 0):
+                return {"error": f"Invalid mid {mid} is below 0. mid cannot be a negative number."}                
+        except ValueError:
+            return {"error": f"Invalid mid: {data['mid']} is not a number"}, 400
+        
+        if (data["semester"] != "Fall") and (data["semester"] != "Spring") and (data["semester"] != "V1") and (data["semester"] != "V2"):
+            return {"error": f"Invalid semester: {data['semester']} is not permitted. Please select Fall, Spring, V1, or V2."}, 400       
+        
+        try:
+            years = int(data["years"])
+            if (years < 0):
+                return {"error": f"Invalid years {years} is below 0. years cannot be a negative number."}               
+        except ValueError:
+            return {"error": f"Invalid years: {data['years']} is not a number"}, 400
+        
+        try:
+            capacity = int(data["capacity"])
+            if (capacity < 1):
+                return {"error": f"Invalid capacity {capacity} is below 0. capacity cannot be bellow 1."}               
+        except ValueError:
+            return {"error": f"Invalid capacity: {data['capacity']} is not a number"}, 400        
+
+        try:
             return self.Courses.InsertSection(data)
         except Exception as e:
             print(f"Insertion error: {e}")
             return {"error": str(e)}, 400
 
     def UpdateSection(self,data):
+        try:
+            sid = int(data["sid"])
+            if (sid < 0):
+                return {"error": f"Invalid sid {sid} is below 0. sid cannot be a negative number."}    
+        except ValueError:
+            return {"error": f"Invalid sid: {data['sid']} is not a number"}, 400
+        
+        try:
+            roomid = int(data["roomid"])
+            if (roomid < 0):
+                return {"error": f"Invalid roomid {roomid} is below 0. roomid cannot be a negative number."}    
+        except ValueError:
+            return {"error": f"Invalid roomid: {data['roomid']} is not a number"}, 400
+        
+        try:
+            cid = int(data["cid"])
+            if (cid < 0):
+                return {"error": f"Invalid cid {cid} is below 0. cid cannot be a negative number."}    
+        except ValueError:
+            return {"error": f"Invalid cid: {data['cid']} is not a number"}, 400
+        
+        try:
+            mid = int(data["mid"])
+            if (mid < 0):
+                return {"error": f"Invalid mid {mid} is below 0. mid cannot be a negative number."}                
+        except ValueError:
+            return {"error": f"Invalid mid: {data['mid']} is not a number"}, 400
+        
+        if (data["semester"] != "Fall") and (data["semester"] != "Spring") and (data["semester"] != "V1") and (data["semester"] != "V2"):
+            return {"error": f"Invalid semester: {data['semester']} is not permitted. Please select Fall, Spring, V1, or V2."}, 400       
+        
+        try:
+            years = int(data["years"])
+            if (years < 0):
+                return {"error": f"Invalid years {years} is below 0. years cannot be a negative number."}               
+        except ValueError:
+            return {"error": f"Invalid years: {data['years']} is not a number"}, 400
+        
+        try:
+            capacity = int(data["capacity"])
+            if (capacity < 1):
+                return {"error": f"Invalid capacity {capacity} is below 0. capacity cannot be bellow 1."}               
+        except ValueError:
+            return {"error": f"Invalid capacity: {data['capacity']} is not a number"}, 400        
+
         try:
             return self.Courses.UpdateSection(data)
         except Exception as e:
@@ -39,6 +122,12 @@ class SectionController:
     def DeleteSectionBySID(self,sid):
         try:
             sid = int(sid)
+        except ValueError:
+            return {"error": f"Invalid type for id: {sid} is not a number"}, 400
+        if (sid < 0):
+            return {"error": f"Invalid id {sid} is below 0. Id cannot be a negative number."}  
+        
+        try:
             return self.Courses.DeleteSectionBySID(sid)
         except Exception as e:
             print(f"Delete error: {e}")
