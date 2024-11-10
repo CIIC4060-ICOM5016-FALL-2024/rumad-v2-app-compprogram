890c7a38a41b8e8947978b0bd50653e5007dd226
benjaminCMD <benjamin.diaz@upr.edu>
2024-11-10 12:29:22 -0400

    Reduced the clutter of if statements

--- Controllers/ClassController.py
+++ Controllers/ClassController.py
@@ -24,57 +24,36 @@ class ClassController:
     
     def InsertClass(self,data):
         try:
-            if type(data["cdesc"]) == str:
-                try:
-                    cdesc = int(data["cdesc"])
-                    return {"error": f"This is not a valid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
-                except:
-                    # it's simply dummy code continue when if the conversion explodes the app
-                    a = 1
-                    
-
             if(data["cname"] != "CIIC") and (data["cname"] != "INSO"):
-                return {"error": f"This is not valid cname: {data['cname']}. Please enter CIIC or INSO"}, 400
-            
-            elif(len(data["cname"]) != 4):
-                return {"error": f"This is a not valid cname"}, 400
+                return {"error": f"Invalid cname: {data['cname']}. Please enter CIIC or INSO"}, 400
             
             try:
                 ccode = str(data["ccode"])  
                 if(len(ccode) != 4 ): 
-                    return {"error" : f"This is not a valid ccode: {data['ccode']}. Please enter a code with only 4 digits"}, 400
+                    return {"error" : f"Invalid ccode: {data['ccode']}. Please enter a code with only 4 digits"}, 400
+                int(ccode)
             except Exception as e:
-                return {"error" : f"This is not a valid ccode: {data['ccode']}. The code can only have numbers"}, 400
+                return {"error" : f"Invalid ccode: {data['ccode']}. The code can only have numbers"}, 400
             
+            if (not type(data["cdesc"]) == str) or data["cdesc"].isdigit():
+                return {"error": f"Invalid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
 
-            if type(data["cdesc"]) == int:
-                return {"error" : f"This is not a valid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
+            if (data["term"]) not in ["None", "First Semester","Second Semester", "First Semester, Second Semester", "According to Demand"]:
+                return {"error" : f"Invalid term: {data['term']}. Please select First Semester, Second Semester, According to Demand or 'First Semester, Second Semester'"}, 400
             
-
-            elif(data["term"] != "None") and (data["term"] != "First Semester") and (data["term"] != "Second Semester") and (data["term"] != "According to Demand") and (data["term"] != "First Semester, Second Semester"):
-                return {"error" : f"This is not a valid term: {data['term']}. Please select First Semester, Second Semester, According to Demand or 'First Semester, Second Semester'"}, 400
+            if (data["years"]) not in ["None", "Every Year","Odd Years", "Even Years", "According to Demand"]:
+                return {"error" : f"Invalid year: {data['years']}. Please pick None, Every Year, Even Years, Odd Years, According to Demand"}, 400
             
-            elif(data["years"] != "None") and (data["years"] != "Every Year") and (data["years"] != "Odd Years") and (data["years"] != "Even Years") and (data["years"] != "According to Demand"):
-                return {"error" : f"This is a not a valid year: {data['years']}. Please pick None, Every Year, Even Years, Odd Years, According to Demand"}, 400
-
-            elif type(data["cred"] == str) or type(data["cred"] == int):
-                try:
-                    cred_str = str(data["cred"])
-                    cred = int(data["cred"])
-                    if(len(cred_str) > 1):
-                        return {"error": f"This is not a valid cred: {data['cred']}. Credits cannot have double digits"}, 400
-                    elif(cred > 5):
-                        return {"error": f"This is not a valid cred: {data['cred']}. Credits be greater than 5"}, 400
-                    elif(cred < 0):
-                        return {"error": f"This is not a valid cred: {data['cred']}. Credits cannot be negative."}, 400
-                except: 
-                    return {"error" : f"This is not a valid cred: {data['cred']}. Credits cannot have letter"}, 400
             try:
-                syllabus = int(data['csyllabus'])
-                return {"error" : f"This is not a valid syllabus: {data['cred']}. Syllabus link cannot be purely numbers"}, 400
-            except Exception as e:
-                # dummy code for the app to continue
-                a = 1
+                cred = int(data["cred"])
+                if not(0 <= cred <= 5):
+                    return {"error": f"Invalid cred: {data['cred']}. Credits must be between 0 to 5"}, 400
+            except ValueError: 
+                return {"error" : f"Invalid cred: {data['cred']}. Credits must be an integer"}, 400
+            
+            
+            if data['csyllabus'].isdigit():
+                return {"error" : f"Invalid syllabus: {data['syllabus']}. Syllabus link cannot be purely numbers"}, 400
                 
             return self.Courses.InsertClass(data)
         except Exception as e:
@@ -82,6 +61,42 @@ class ClassController:
 
     def UpdateClass(self,cid,data):
         try:
+            try:
+                cid = int(cid)
+            except ValueError:
+                return {"error": f"Invalid cid: {cid} is not a number"}, 400
+            
+            if(data["cname"] != "CIIC") and (data["cname"] != "INSO"):
+                return {"error": f"Invalid cname: {data['cname']}. Please enter CIIC or INSO"}, 400
+            
+            try:
+                ccode = str(data["ccode"])  
+                if(len(ccode) != 4 ): 
+                    return {"error" : f"Invalid ccode: {data['ccode']}. Please enter a code with only 4 digits"}, 400
+                int(ccode)
+            except Exception as e:
+                return {"error" : f"Invalid ccode: {data['ccode']}. The code can only have numbers"}, 400
+            
+            if not type(data["cdesc"]) == str or data["cdesc"].isdigit():
+                return {"error": f"Invalid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
+
+            if (data["term"]) not in ["None", "First Semester","Second Semester", "First Semester, Second Semester", "According to Demand"]:
+                return {"error" : f"Invalid term: {data['term']}. Please select First Semester, Second Semester, According to Demand or 'First Semester, Second Semester'"}, 400
+            
+            if (data["years"]) not in ["None", "Every Year","Odd Years", "Even Years", "According to Demand"]:
+                return {"error" : f"Invalid year: {data['years']}. Please pick None, Every Year, Even Years, Odd Years, According to Demand"}, 400
+            
+            try:
+                cred = int(data["cred"])
+                if not(0 <= cred <= 5):
+                    return {"error": f"Invalid cred: {data['cred']}. Credits must be between 0 to 5"}, 400
+            except ValueError: 
+                return {"error" : f"Invalid cred: {data['cred']}. Credits must be an integer"}, 400
+            
+            
+            if not type(data['csyllabus']) == str or data['csyllabus'].isdigit():
+                return {"error" : f"Invalid syllabus: {data['csyllabus']}. Syllabus link cannot be purely numbers"}, 400
+            
             return self.Courses.UpdateClass(cid,data)
         except Exception as e:
             print(f"Update error: {e}")
@@ -90,6 +105,8 @@ class ClassController:
     def DeleteClassByCID(self,cid):
         try:
             cid = int(cid)
+            if (cid < 2):
+                return {"error": f"Invalid cid: {cid}. The cid must be greater than 1"}, 400
             return self.Courses.DeleteClassByCID(cid)
         except Exception as e:
             print(f"Delete error: {e}")
