4fc6664ba648fb6dddd42858b629ad7fae711359
benjaminCMD <benjamin.diaz@upr.edu>
2024-11-09 20:28:45 -0400

    Implemented error handling for class insert

--- Controllers/ClassController.py
+++ Controllers/ClassController.py
@@ -24,6 +24,14 @@ class ClassController:
     
     def InsertClass(self,data):
         try:
+            if type(data["cdesc"]) == str:
+                try:
+                    cdesc = int(data["cdesc"])
+                    return {"error": f"This is not a valid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
+                except:
+                    # it's simply dummy code continue when if the conversion explodes the app
+                    a = 1
+                    
 
             if(data["cname"] != "CIIC") and (data["cname"] != "INSO"):
                 return {"error": f"This is not valid cname: {data['cname']}. Please enter CIIC or INSO"}, 400
@@ -32,21 +40,44 @@ class ClassController:
                 return {"error": f"This is a not valid cname"}, 400
             
             try:
-                ccode = int(data["ccode"])
-                if(len(data["ccode"]) != 4 ): 
+                ccode = str(data["ccode"])  
+                if(len(ccode) != 4 ): 
                     return {"error" : f"This is not a valid ccode: {data['ccode']}. Please enter a code with only 4 digits"}, 400
             except Exception as e:
                 return {"error" : f"This is not a valid ccode: {data['ccode']}. The code can only have numbers"}, 400
             
-            if(isinstance(int(data["cdesc"]), int)):
+
+            if type(data["cdesc"]) == int:
                 return {"error" : f"This is not a valid cdesc: {data['cdesc']}. The cdesc needs to be a string"}, 400
             
+
             elif(data["term"] != "None") and (data["term"] != "First Semester") and (data["term"] != "Second Semester") and (data["term"] != "According to Demand") and (data["term"] != "First Semester, Second Semester"):
                 return {"error" : f"This is not a valid term: {data['term']}. Please select First Semester, Second Semester, According to Demand or 'First Semester, Second Semester'"}, 400
+            
+            elif(data["years"] != "None") and (data["years"] != "Every Year") and (data["years"] != "Odd Years") and (data["years"] != "Even Years") and (data["years"] != "According to Demand"):
+                return {"error" : f"This is a not a valid year: {data['years']}. Please pick None, Every Year, Even Years, Odd Years, According to Demand"}, 400
 
+            elif type(data["cred"] == str) or type(data["cred"] == int):
+                try:
+                    cred_str = str(data["cred"])
+                    cred = int(data["cred"])
+                    if(len(cred_str) > 1):
+                        return {"error": f"This is not a valid cred: {data['cred']}. Credits cannot have double digits"}, 400
+                    elif(cred > 5):
+                        return {"error": f"This is not a valid cred: {data['cred']}. Credits be greater than 5"}, 400
+                    elif(cred < 0):
+                        return {"error": f"This is not a valid cred: {data['cred']}. Credits cannot be negative."}, 400
+                except: 
+                    return {"error" : f"This is not a valid cred: {data['cred']}. Credits cannot have letter"}, 400
+            try:
+                syllabus = int(data['csyllabus'])
+                return {"error" : f"This is not a valid syllabus: {data['cred']}. Syllabus link cannot be purely numbers"}, 400
+            except Exception as e:
+                # dummy code for the app to continue
+                a = 1
+                
             return self.Courses.InsertClass(data)
         except Exception as e:
-            print(f"Insertion error: {e}")
             return {"error": str(e)}, 400
 
     def UpdateClass(self,data):
