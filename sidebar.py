db0fa42a5dbc20236bc63e24e51bdb0efd462933
rafa-quiles <rafael.quiles1@upr.edu>
2024-12-06 23:53:40 -0400

    added logo to sidebar and made size set

--- sidebar.py
+++ sidebar.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
 def make_sidebar():
+
+    st.markdown(
+    """
+    <style>
+        section[data-testid="stSidebar"] {
+            width: 200px !important; # Set the width to your desired value
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+    st.sidebar.image("seal-rum-uprm-1280x1280px.png", width = 190)
+
     st.sidebar.page_link("Main_Page.py", label="Main Page")
     st.sidebar.page_link("pages/chatbot.py", label="Chatbot")
     st.sidebar.page_link("pages/LeastClassTaughtPage.py", label="Least Taught Class")
@@ -11,3 +25,4 @@ def make_sidebar():
     st.sidebar.page_link("pages/MostClassPerSemesterYearPage.py", label="Most Class Per Semester")
     st.sidebar.page_link("pages/MostPrerequisitePage.py", label="Most Prerequisite")
     st.sidebar.page_link("pages/SectionsPerYearPage.py", label="Sections Per Year")
+
